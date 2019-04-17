f6abce5ba2f5284c5a82c5c9fa6000e4ca33eeaf
Vikrant Godse <vikrant.g@amazatic.com>
2019-04-17 22:15:00 +0530

    Admin changes.

--- admin.py
+++ admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 # Register your models here.
+#this is testingxs
 from .models import AllLifeHacks
 
 admin.site.register(AllLifeHacks)
+
+
+# this is for testing
